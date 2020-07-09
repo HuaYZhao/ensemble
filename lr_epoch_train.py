4f973a153c732418a3b96029aadfc923d6be77a4
HuaYZhao <921203405@qq.com>
2020-07-09 09:55:27 +0800

    gasen

--- lr_epoch_train.py
+++ lr_epoch_train.py
@@ -25,7 +25,7 @@ class myThread(threading.Thread):
 def per_tpu_run(tpu_id):
     for lr in LrRange:
         for epoch in EpochRange:
-            print(f"train lr: {lr}, epoch: {epoch}, tpu: {TPU_NAMES[tpu_id]}")
+            print(f"train lr: {lr}, epoch: {epoch}, tpu: {TPU_NAMES[tpu_id - 1]}")
 
             xargs = f"""cd ../atrlp && python3 run_finetuning.py   --data-dir=gs://squad_cx/electra_data{tpu_id} --model-name=electra_large   --hparams '{{"model_size": "large", "task_names": ["squad"], "num_train_epochs": {epoch}, "use_tpu": true, "num_tpu_cores": 8, "tpu_name": "{TPU_NAMES[tpu_id - 1]}", "train_batch_size": 32, "eval_batch_size": 32, "predict_batch_size": 32, "max_seq_length": 512, "learning_rate": {lr}, "use_tfrecords_if_existing": true, "num_trials": 1, "do_train": true, "do_eval": true, "save_checkpoints_steps": 100000 }}' """
             os.system(xargs)

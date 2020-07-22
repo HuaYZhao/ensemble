1fee84a9038a841f785dca6bca6c4f1bbe2b8e1e
HuaYZhao <921203405@qq.com>
2020-07-22 17:22:29 +0800

    gasen

--- export.py
+++ export.py
@@ -34,27 +34,12 @@ def export(checkpoint, export_path, tpu_address):
         saver.restore(sess, checkpoint)
         my_vars = []
         for var in tf.global_variables():
+            print(var.name)
             if "adam_v" not in var.name and "adam_m" not in var.name:
                 my_vars.append(var)
         new_saver = tf.train.Saver(my_vars)
         new_saver.save(sess, output_ckpt)
 
 
-def frozen_pb(checkpoint, export_path, tpu_address):
-    output_pb = os.path.join(export_path, "frozen_model.pb")
-    tf.reset_default_graph()
-
-    saver = tf.train.import_meta_graph(checkpoint + '.meta', clear_devices=True)
-    with tf.Session(tpu_address) as sess:
-        saver.restore(sess, checkpoint)
-
-        output_graph_def = convert_variables_to_constants(sess, sess.graph_def,
-                                                          output_node_names=['answer_class/Squeeze',
-                                                                             'start_logits/LogSoftmax',
-                                                                             'end_logits/LogSoftmax'])
-        with tf.gfile.FastGFile(output_pb, mode='wb') as f:
-            f.write(output_graph_def.SerializeToString())
-
-
 if __name__ == '__main__':
-    frozen_pb(FLAGS.checkpoint, FLAGS.export_path, FLAGS.tpu_address)
+    export(FLAGS.checkpoint, FLAGS.export_path, FLAGS.tpu_address)
